9faa467e70d6109a734c8ff96da999b9b58a461c
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-12-11 15:07:45 -0500

    Refactor `fetch.py` and `main.py`.
    This commit simplifies the logic in `get_papers()`, `parse_response()`,
    and `main()`. We use lazy iteration instead of loops to cut down on
    boilerplate and to make things slightly faster.
    
    The publication date is used everywhere instead of a mix of the last
    updated date and publication date, under the assumption that papers
    don't change enough to warrant re-rating after initial publication.
    
    We move the query construction into the top-level to separate it from
    the downloading logic, and we move the start date determination into
    `main()` from `get_papers()`. The `init` date is now defaulted to 3 days
    ago in the absence of a checkpoint/save.
    
    * Fix missing stuff and typo in `requirements.txt`.
    * Rewrite `Paper.__repr__()` and use it in `interface.py`.
    * Drop `batch_size` argument as it is unnecessary for the moment.
    * Add an immutable `Save` dataclass to reduce boilerplate in the
      checkpointing procedure.

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 
 import os
 import pickle
+from typing import Dict, Union
+from dataclasses import dataclass
+
+import datetime
+from datetime import timedelta
 
 from fetch import get_papers
 from interface import get_ratings
@@ -9,45 +14,59 @@ from recommender import recommended_sort, train_recommender
 from utils import USER_DATA_PATH
 
 
+@dataclass(frozen=True, eq=True)
+class Save:
+    ratings: Dict[str, float]
+    date: Union[datetime.date, None]
+    init: datetime.date
+
+
 def main():
+    """
+    Get a list of arXiv papers released on or after the later of ``init`` and
+    ``save.date`` minus 14 days.
+
+    Note that ``save.date`` is the last date that the user "caught up" and
+    rated all papers which were currently available at the time of rating. This
+    is a bit janky, but it's the cleanest way I've thought of to deal with the
+    fact that the arXiv API only dates papers by when they were submitted, but
+    not all papers which are submitted on the same day are released on the same
+    day.
+
+    The 14 day cushion accounts for the event where we rate papers which were
+    submitted on a given day, and sometime after there are other papers
+    released which were submitted on that same day.
+    """
 
     # Read database of ratings.
+    save = Save(ratings={}, date=None, init=datetime.date.today() - timedelta(days=3))
     if os.path.isfile(USER_DATA_PATH):
         with open(USER_DATA_PATH, "rb") as f:
-            user_data = pickle.load(f)
-        ratings = user_data["ratings"]
-        checkpoint = user_data["checkpoint"]
-        init_date = user_data["init_date"]
-    else:
-        ratings = {}
-        checkpoint = None
-        init_date = None
+            save = pickle.load(f)
+    init = save.init
 
-    # Fetch arXiv releases since checkpoint and keep those that haven't yet been rated.
-    batch_papers, init_date = get_papers(init_date=init_date, checkpoint=checkpoint)
-    unrated_papers = [p for p in batch_papers if p.identifier not in ratings.keys()]
+    # Construct start date for papers.
+    start = init if save.date is None else max(init, save.date - timedelta(days=14))
+
+    # Fetch arXiv releases since ``save.date`` and keep those that haven't been rated.
+    papers = get_papers(start)
+    unrated_papers = [p for p in papers if p.identifier not in save.ratings.keys()]
 
     # Sort unrated papers by predicted rating and present to user for rating.
     unrated_papers, pred_ratings = recommended_sort(unrated_papers)
-    batch_ratings = get_ratings(
-        unrated_papers, pred_ratings, len(unrated_papers), init_date, checkpoint
-    )
-    finished = (len(batch_ratings) == len(unrated_papers))
+    batch_ratings = get_ratings(unrated_papers, pred_ratings, init, save.date)
+    finished = len(batch_ratings) == len(unrated_papers)
 
     # Update database of ratings.
-    ratings.update(batch_ratings)
-    parent = os.path.dirname(USER_DATA_PATH)
-    if not os.path.isdir(parent):
-        os.makedirs(parent)
+    ratings = {**save.ratings, **batch_ratings}
+    latest = init
+    if len(ratings) > 0 and finished:
+        latest = max(p.published for p, _ in ratings.values())
+
+    # Serialize checkpoint.
+    os.makedirs(os.path.dirname(USER_DATA_PATH), exist_ok=True)
     with open(USER_DATA_PATH, "wb") as f:
-        if len(ratings) > 0 and finished:
-            checkpoint = max([p.published for (p, _) in ratings.values()])
-        user_data = {
-            "ratings": ratings,
-            "checkpoint": checkpoint,
-            "init_date": init_date
-        }
-        pickle.dump(user_data, f)
+        pickle.dump(Save(ratings=ratings, date=latest, init=init), f)
 
     # Retrain SVM with new ratings.
     if len(ratings) > 0:
